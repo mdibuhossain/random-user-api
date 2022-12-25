d5993c4c789b7b68e035f0b1b3c6a9313dbb86f6
chadnipodder <chadnipodder@gmail.com>
2022-12-26 02:11:01 +0600

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,3 +12,43 @@ exports.getRandomUser = (req, res) => {
   const rand = Math.floor(Math.random() * users.length);
   res.status(200).json({ data: users[rand] });
 };
+
+exports.saveUser = (req, res) => {
+  try {
+    const data = fs.readFileSync(process.cwd() + "/data/users.json");
+    const users = JSON.parse(data);
+    const tmpUser = {
+      id: users.length + 1,
+      name: req.body.name,
+      gender: req.body.gender,
+      address: req.body.address,
+      contact: req.body.contact,
+      photoUrl: req.body.photoUrl,
+    };
+    if (!tmpUser.name) {
+      return res.status(400).json({ error: "Name required." });
+    }
+    if (!tmpUser.gender) {
+      return res.status(400).json({ error: "Gender required." });
+    }
+    if (!tmpUser.address) {
+      return res.status(400).json({ error: "Address required." });
+    }
+    if (!tmpUser.contact) {
+      return res.status(400).json({ error: "Contact required." });
+    }
+    if (!tmpUser.photoUrl) {
+      return res.status(400).json({ error: "photoUrl required." });
+    }
+    users.push(tmpUser);
+    fs.writeFile(
+      process.cwd() + "/data/users.json",
+      JSON.stringify(users, null, 2),
+      () => {
+        res.status(200).json({ data: tmpUser });
+      }
+    );
+  } catch {
+    res.status(500).json({ error: "Somthing went wrong!" });
+  }
+};
--- routers/user.router.js
+++ routers/user.router.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getAllUsers,
   getRandomUser,
+  saveUser,
 } = require("../controllers/user.controller.js");
 
 const router = express.Router();
 
 router.get("/all", getAllUsers);
 router.get("/random", getRandomUser);
+router.post("/save", saveUser);
 
 module.exports = router;
